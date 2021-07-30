2d665014e63f0b94d146d1a3845e1706b4adc5e7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-30 12:15:38 +0100

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:covid_19_ui/widgets/counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'widgets/header.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -33,62 +35,10 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              padding: EdgeInsets.only(
-                top: 50,
-                left: 40,
-                right: 20,
-              ),
-              height: 350,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFF3383CD),
-                    Color(0xFF11249F),
-                  ],
-                ),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/virus.png'),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: SvgPicture.asset('assets/icons/menu.svg'),
-                  ),
-                  SizedBox(height: 20),
-                  Expanded(
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icons/Drcorona.svg',
-                          width: 230,
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter,
-                        ),
-                        Positioned(
-                          top: 20,
-                          left: 150,
-                          child: Text(
-                            'All you need \nis stay at home',
-                            style:
-                                kHeadingTextStyle.copyWith(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+          MyHeader(
+            imagePath: 'assets/icons/Drcorona.svg',
+            textTop: 'All you need',
+            textBottom: 'is stay at home!!',
           ),
           Container(
             height: 60,
@@ -239,23 +189,5 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
---
+++ lib/widgets/header.dart
@@ -0,0 +1,92 @@
+import 'package:covid_19_ui/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyHeader extends StatelessWidget {
+  final String imagePath;
+  final String textTop, textBottom;
+  const MyHeader({
+    Key? key,
+    required this.imagePath,
+    required this.textTop,
+    required this.textBottom,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 50,
+          left: 40,
+          right: 20,
+        ),
+        height: 350,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF3383CD),
+              Color(0xFF11249F),
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage(imagePath),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: SvgPicture.asset('assets/icons/menu.svg'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/Drcorona.svg',
+                    width: 230,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+                  Positioned(
+                    top: 20,
+                    left: 150,
+                    child: Text(
+                      '$textTop \n$textBottom',
+                      style: kHeadingTextStyle.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
