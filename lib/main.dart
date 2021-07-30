788c5f2bd15daea1abd09cf757def9d812a0a475
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-30 12:00:01 +0100

    Statistics

--- lib/main.dart
+++ lib/main.dart
@@ -30,105 +30,228 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        ClipPath(
-          clipper: MyClipper(),
-          child: Container(
-            padding: EdgeInsets.only(
-              top: 50,
-              left: 40,
-              right: 20,
-            ),
-            height: 350,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Color(0xFF3383CD),
-                  Color(0xFF11249F),
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(
+                top: 50,
+                left: 40,
+                right: 20,
+              ),
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/virus.png'),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset('assets/icons/menu.svg'),
+                  ),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/Drcorona.svg',
+                          width: 230,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          top: 20,
+                          left: 150,
+                          child: Text(
+                            'All you need \nis stay at home',
+                            style:
+                                kHeadingTextStyle.copyWith(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-              image: DecorationImage(
-                image: AssetImage('assets/images/virus.png'),
-              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+          Container(
+            height: 60,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0xFFE5E5E5),
+                )),
+            child: Row(
               children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: SvgPicture.asset('assets/icons/menu.svg'),
+                SvgPicture.asset('assets/icons/maps-and-flags.svg'),
+                SizedBox(
+                  width: 20,
                 ),
-                SizedBox(height: 20),
                 Expanded(
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/Drcorona.svg',
-                        width: 230,
-                        fit: BoxFit.fitWidth,
-                        alignment: Alignment.topCenter,
-                      ),
-                      Positioned(
-                        top: 20,
-                        left: 150,
-                        child: Text(
-                          'All you need \nis stay at home',
-                          style:
-                              kHeadingTextStyle.copyWith(color: Colors.white),
-                        ),
-                      ),
-                    ],
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    icon: SvgPicture.asset('assets/icons/dropdown.svg'),
+                    items: (['Nigeria', 'London', 'Germany', 'Canada', 'Spain']
+                        .map<DropdownMenuItem<String>>((value) =>
+                            DropdownMenuItem(value: value, child: Text(value)))
+                        .toList()),
+                    onChanged: (value) {},
+                    value: 'Nigeria',
                   ),
-                )
+                ),
               ],
             ),
           ),
-        ),
-        Container(
-          height: 60,
-          width: double.infinity,
-          margin: EdgeInsets.symmetric(
-            horizontal: 20,
+          SizedBox(
+            height: 20,
           ),
-          padding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 20,
-          ),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: Color(0xFFE5E5E5),
-              )),
-          child: Row(
-            children: [
-              SvgPicture.asset('assets/icons/maps-and-flags.svg'),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: DropdownButton(
-                  isExpanded: true,
-                  underline: SizedBox(),
-                  icon: SvgPicture.asset('assets/icons/dropdown.svg'),
-                  items: (['Nigeria', 'London', 'Germany', 'Canada', 'Spain']
-                      .map<DropdownMenuItem<String>>((value) =>
-                          DropdownMenuItem(value: value, child: Text(value)))
-                      .toList()),
-                  onChanged: (value) {},
-                  value: 'Nigeria',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Case Update\n",
+                            style: kTitleTextStyle,
+                          ),
+                          TextSpan(
+                            text: "Newest update : July 30th\n",
+                            style: kTitleTextStyle,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'See details',
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Counter(
+                        color: kInfectedColor,
+                        number: 1892,
+                        title: 'Infected',
+                      ),
+                      Counter(
+                        color: kDeathColor,
+                        number: 12,
+                        title: 'Deaths',
+                      ),
+                      Counter(
+                        color: kRecoveryColor,
+                        number: 1754,
+                        title: 'Recovered',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  final int number;
+  final Color color;
+  final String title;
+  const Counter({
+    Key? key,
+    required this.number,
+    required this.color,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            color: color.withOpacity(.6),
+            shape: BoxShape.circle,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(
+                color: color,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          '$number',
+          style: TextStyle(
+            fontSize: 40,
+            color: color,
+          ),
+        ),
+        Text(
+          title,
+          style: kSubTextStyle,
         ),
       ],
-    ));
+    );
   }
 }
 
