75188f6bc1a6f6dbee654efe8f592ee5e27b9dec
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-30 13:10:03 +0100

    HomePage Completed

--- lib/main.dart
+++ lib/main.dart
@@ -33,160 +33,169 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          MyHeader(
-            imagePath: 'assets/icons/Drcorona.svg',
-            textTop: 'All you need',
-            textBottom: 'is stay at home!!',
-          ),
-          Container(
-            height: 60,
-            width: double.infinity,
-            margin: EdgeInsets.symmetric(
-              horizontal: 20,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MyHeader(
+              imagePath: 'assets/icons/Drcorona.svg',
+              textTop: 'All you need',
+              textBottom: 'is stay at home!!',
             ),
-            padding: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 20,
-            ),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(
-                  color: Color(0xFFE5E5E5),
-                )),
-            child: Row(
-              children: [
-                SvgPicture.asset('assets/icons/maps-and-flags.svg'),
-                SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: DropdownButton(
-                    isExpanded: true,
-                    underline: SizedBox(),
-                    icon: SvgPicture.asset('assets/icons/dropdown.svg'),
-                    items: (['Nigeria', 'London', 'Germany', 'Canada', 'Spain']
-                        .map<DropdownMenuItem<String>>((value) =>
-                            DropdownMenuItem(value: value, child: Text(value)))
-                        .toList()),
-                    onChanged: (value) {},
-                    value: 'Nigeria',
+            Container(
+              height: 60,
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Color(0xFFE5E5E5),
+                  )),
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/icons/maps-and-flags.svg'),
+                  SizedBox(
+                    width: 20,
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: DropdownButton(
+                      isExpanded: true,
+                      underline: SizedBox(),
+                      icon: SvgPicture.asset('assets/icons/dropdown.svg'),
+                      items: ([
+                        'Nigeria',
+                        'London',
+                        'Germany',
+                        'Canada',
+                        'Spain'
+                      ]
+                          .map<DropdownMenuItem<String>>((value) =>
+                              DropdownMenuItem(
+                                  value: value, child: Text(value)))
+                          .toList()),
+                      onChanged: (value) {},
+                      value: 'Nigeria',
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "Case Update\n",
-                            style: kTitleTextStyle,
-                          ),
-                          TextSpan(
-                            text: "Newest update : July 30th\n",
-                            style: kTitleTextStyle,
-                          ),
-                        ],
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Case Update\n",
+                              style: kTitleTextStyle,
+                            ),
+                            TextSpan(
+                              text: "Newest update : July 30th\n",
+                              style: kTitleTextStyle,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Text(
-                      'See details',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 4),
-                        blurRadius: 30,
-                        color: kShadowColor,
+                      Spacer(),
+                      Text(
+                        'See details',
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ],
                   ),
-                  child: Row(
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 4),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Counter(
+                          color: kInfectedColor,
+                          number: 1892,
+                          title: 'Infected',
+                        ),
+                        Counter(
+                          color: kDeathColor,
+                          number: 12,
+                          title: 'Deaths',
+                        ),
+                        Counter(
+                          color: kRecoveryColor,
+                          number: 1754,
+                          title: 'Recovered',
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Counter(
-                        color: kInfectedColor,
-                        number: 1892,
-                        title: 'Infected',
+                      Text(
+                        'Spread of Virus',
+                        style: kTitleTextStyle,
                       ),
-                      Counter(
-                        color: kDeathColor,
-                        number: 12,
-                        title: 'Deaths',
-                      ),
-                      Counter(
-                        color: kRecoveryColor,
-                        number: 1754,
-                        title: 'Recovered',
+                      Text(
+                        'See details',
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ],
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Spread of Virus',
-                      style: kTitleTextStyle,
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
                     ),
-                    Text(
-                      'See details',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.all(20),
-                  height: 178,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 30,
-                        color: kShadowColor,
-                      ),
-                    ],
-                  ),
-                  child: Image.asset('assets/images/map.png'),
-                )
-              ],
+                    child: Image.asset('assets/images/map.png'),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
